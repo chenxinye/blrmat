e8d6fffa66d10b6fdb51b10918dbf57901dbbc30
chenxinye <cclcquant@yahoo.com>
2025-12-11 19:47:02 +0100

    init commit

---
+++ build/CMakeFiles/blr_core.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blr_core.dir/src/blr_matrix.cu.o"
+  "CMakeFiles/blr_core.dir/src/blr_matrix.cu.o.d"
+  "libblr_core.a"
+  "libblr_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/blr_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/blr_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blr_test.dir/main_test.cu.o"
+  "CMakeFiles/blr_test.dir/main_test.cu.o.d"
+  "blr_test"
+  "blr_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/blr_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
